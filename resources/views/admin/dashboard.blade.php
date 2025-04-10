b8fb186ba0b9d98ce9684e5e91e0a3bb4e3bd7c7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-10 16:25:12 +0100

    refactor admin authentication and dashboard layout for improved structure and usability

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,13 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+{{-- <x-app-layout> --}}
+    @extends('layouts.admin-app')
+    @section('content')
+    {{-- <x-slot name="header"> --}}
+        <div class="header max-w-7xl mx-auto sm:px-6 lg:px-8 p-6">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Admin Dashboard') }}
+            </h2>
+        </div>
+    {{-- </x-slot> --}}
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -14,4 +18,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+    @endsection
+{{-- </x-app-layout> --}}
