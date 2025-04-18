34fbf7aac76574182e2e9557c56e5c35c10739ad
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-18 15:35:14 +0100

    enhance student dashboard; add rejection alert for instructor requests and improve layout formatting

--- resources/views/frontend/student-dashboard/index.blade.php
+++ resources/views/frontend/student-dashboard/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--===========================
-            BREADCRUMB START
-        ============================-->
+                BREADCRUMB START
+            ============================-->
     <section class="wsus__breadcrumb" style="background: url(images/breadcrumb_bg.jpg);">
         <div class="wsus__breadcrumb_overlay">
             <div class="container">
@@ -22,20 +22,20 @@
         </div>
     </section>
     <!--===========================
-            BREADCRUMB END
-        ============================-->
+                BREADCRUMB END
+            ============================-->
 
 
     <!--===========================
-            DASHBOARD OVERVIEW START
-        ============================-->
+                DASHBOARD OVERVIEW START
+            ============================-->
     <section class="wsus__dashboard mt_90 xs_mt_70 pb_120 xs_pb_100">
         <div class="container">
             <div class="row">
                 @include('frontend.student-dashboard.sidebar')
 
 
-                
+
                 <div class="col-xl-9 col-md-8">
                     @if (auth()->user()->approval_status == 'pending')
                         <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -64,15 +64,44 @@
                                 will recieve a notification once it is approved.
                             </div>
                         </div>
+                    @elseif (auth()->user()->approval_status == 'rejected')
+                        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                <path
+                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                            </symbol>
+                            <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                                <path
+                                    d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                            </symbol>
+                            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </symbol>
+                        </svg>
+
+
+                        <div class="alert alert-danger d-flex align-items-center" role="alert">
+                            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img"
+                                aria-label="Info:">
+                                <use xlink:href="#info-fill" />
+                            </svg>
+                            <div>
+                                Hi, {{ auth()->user()->name }}, your Instructor request has been rejected. If you believe
+                                this was an error or you'd like to appeal or provide more information, feel free to contact
+                                our support team.
+                            </div>
+                        </div>
                     @endif
-                    
+
 
                     @if (auth()->user()->approval_status !== 'pending')
                         <div class="text-end">
-                            <a href="{{ route('student.become-instructor') }}" class="btn btn-primary">Become an Instructor</a>
+                            <a href="{{ route('student.become-instructor') }}" class="btn btn-primary">Become an
+                                Instructor</a>
                         </div>
                     @endif
-                    
+
                     <div class="row">
                         <div class="col-xl-4 col-sm-6 wow fadeInUp">
                             <div class="wsus__dash_earning">
@@ -97,12 +126,12 @@
                         </div>
                     </div>
 
-                    </div>
                 </div>
             </div>
         </div>
+        </div>
     </section>
     <!--===========================
-            DASHBOARD OVERVIEW END
-        ============================-->
+                DASHBOARD OVERVIEW END
+            ============================-->
 @endsection
