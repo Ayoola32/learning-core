5199847f81174904a0af5e74b0efa39c2db9fb19
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-23 14:11:50 +0100

    Refactor action buttons in Course Language and Course Level DataTables to use icon fonts instead of SVGs; update back navigation buttons in create and edit views for consistency.

--- resources/views/admin/course/course-language/index.blade.php
+++ resources/views/admin/course/course-language/index.blade.php
@@ -11,13 +11,7 @@
                             <!-- Page title actions -->
                             <div class="card-actions">
                                 <a href="{{ route('admin.course-language.create')}}" class="btn btn-primary btn-3">
-                                    <!-- Download SVG icon from http://tabler.io/icons/icon/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
-                                        <path d="M12 5l0 14"></path>
-                                        <path d="M5 12l14 0"></path>
-                                    </svg>
+                                    <i class="ti ti-plus"></i>
                                     Add new
                                 </a>
                             </div>
