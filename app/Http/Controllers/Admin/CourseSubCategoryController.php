6732169084d0ab831307079962a30e98d41378e6
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-25 00:45:23 +0100

    Add CourseSubCategoryStoreRequest for validation and enhance store method in CourseSubCategoryController

--- app/Http/Controllers/Admin/CourseSubCategoryController.php
+++ app/Http/Controllers/Admin/CourseSubCategoryController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\CourseSubCategoryDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CourseSubCategoryStoreRequest;
 use App\Models\CourseCategory;
+use App\Models\CourseSubCategory;
 use Illuminate\Http\Request;
+use App\Traits\FileUpload;
+use Illuminate\Support\Str;
 
 class CourseSubCategoryController extends Controller
 {
+    use FileUpload;
     public function index(CourseCategory $course_category, CourseSubCategoryDataTable $dataTable)
     {
 
@@ -26,9 +31,29 @@ class CourseSubCategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CourseSubCategoryStoreRequest $request, CourseCategory $course_category)
     {
-        //
+        $subCategory = new CourseSubCategory();
+
+        if($request->hasFile('image')){
+            $imagePath = $this->uploadFile($request->file('image'), 'uploads/course-category');
+            $subCategory->image = $imagePath;
+        }
+        $subCategory->name = $request->name;
+        $subCategory->slug = Str::slug($request->name);
+        $subCategory->icon = $request->icon;
+        $subCategory->status = $request->status;
+        $subCategory->show_at_trending = $request->show_at_trending;
+        $subCategory->category_id = $course_category->id;
+
+        // Prevent enabling show_at_trending if status is off
+        if ($subCategory->status == 0 && $subCategory->show_at_trending == 1) {
+            $subCategory->show_at_trending = 0;
+            session()->flash('warning', 'Show at Trending was turned off because Status is off');
+        }
+        
+        $subCategory->save();
+        return redirect()->route('admin.sub-category.index', $course_category->id)->with('success', 'Course Sub Category Created Successfully');
     }
 
     /**
