81733663a8729ebedfb821d1a1294ffcbfd71bcd
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-25 16:49:43 +0100

    Implement course index view and update CourseController to return the view

--- app/Http/Controllers/Frontend/CourseController.php
+++ app/Http/Controllers/Frontend/CourseController.php
@@ -12,7 +12,7 @@ class CourseController extends Controller
      */
     public function index()
     {
-        return 'course index';
+        return view('frontend.instructor-dashboard.course.index');
     }
 
     /**
