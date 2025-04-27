a9a49d099942f52d6145085017833443dea3f539
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-27 23:23:36 +0100

    Create migration for courses table with necessary fields and relationships

---
+++ database/migrations/2025_04_25_203728_create_courses_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('course_categories');
+            $table->enum('course_type', ['course'])->default('course');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('seo_description')->nullable();
+            $table->string('duration')->nullable();
+            $table->string('time_zone')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->enum('demo_video_storage', ['upload', 'youtube', 'vimeo', 'external_link'])->nullable();
+            $table->text('description')->nullable();
+            $table->integer('capacity')->nullable();
+            $table->double('price')->nullable();
+            $table->double('discount')->nullable();
+            $table->boolean('certificate')->nullable()->default(1);
+            $table->boolean('qna')->default(1);
+            $table->text('message_for_reviewer')->nullable();
+            $table->enum('is_approved', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->enum('status', ['draft', 'active', 'inactive'])->default('draft');
+            $table->foreignId('course_level_id')->nullable();
+            $table->foreignId('course_language_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
