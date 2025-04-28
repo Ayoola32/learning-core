912ddc2d044a452f3ab099e80d01941ae1e7f0e6
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-28 20:33:12 +0100

    coursebasicinfo request created and added an array of validated columns

---
+++ app/Http/Requests/Frontend/CourseBasicInfoCreateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Frontend;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CourseBasicInfoCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required','string', 'max:255'],
+            'seo_description' => ['nullable', 'string', 'max:255'],
+            'demo_video_storage' => ['nullable', 'string', 'in:upload,vimeo,youtube,external_link'],
+            'demo_video_source' => ['nullable', 'string', 'max:255'],
+            'price' => ['required', 'numeric', 'min:0'],
+            'discount_price' => ['nullable', 'numeric', 'min:0'],
+            'description' => ['required', 'string'],
+            'thumbnail' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+        ];
+    }
+}
