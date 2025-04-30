76893333269db2b968806f0ef76ac6b32668db04
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-04-30 12:13:16 +0100

    second page for the course filling, and fetch courses attributes like langugages, level and categories dynamically

---
+++ resources/views/frontend/instructor-dashboard/course/more-info.blade.php
@@ -0,0 +1,88 @@
+@extends('frontend.instructor-dashboard.course.course-app')
+
+@section('course-content')
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+        <div class="add_course_basic_info">
+            <form action="{{ route('instructor.courses.update', $course->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="row">
+                    <div class="col-xl-6">
+                        <div class="add_course_more_info_input">
+                            <label for="#">Capacity</label>
+                            <input type="text" name="capacity" placeholder="Capacity">
+                            <p>leave blank for unlimited</p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6">
+                        <div class="add_course_more_info_input">
+                            <label for="#">Course Duration (Minutes)*</label>
+                            <input type="text" name="duration" placeholder="300">
+                        </div>
+                    </div>
+                    <div class="col-xl-6">
+                        <div class="add_course_more_info_checkbox">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="qna" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">Q&A</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="certificate" value="" id="flexCheckDefault2">
+                                <label class="form-check-label" for="flexCheckDefault2">Completion Certificate</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="add_course_more_info_input">
+                            <label for="#">Category *</label>
+                            <select class="select_2" name="category">
+                                <option value=""> Please Select </option>
+                                @foreach ($categories as $category)
+                                    @if ($category->subCategories->isNotEmpty())
+                                        <optgroup label="{{ $category->name }}">
+                                            @foreach ($category->subCategories as $subCategory)
+                                                <option value="{{ $subCategory->id }}">{{ $subCategory->name }}</option>
+                                            @endforeach
+                                        </optgroup>
+                                    @endif
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-xl-4">
+                        <div class="add_course_more_info_radio_box">
+                            <h3>Level</h3>
+                            @foreach ($courseLevels as $level)
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="level"
+                                        id="id-{{ $level->id }}">
+                                    <label class="form-check-label" for="id-{{ $level->id }}">
+                                        {{$level->name}}
+                                    </label>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="col-xl-4">
+                        <div class="add_course_more_info_radio_box">
+                            <h3>Language</h3>
+                            @foreach ($courseLanguages as $language)
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="language"
+                                        id="id-{{$language->id}}">
+                                    <label class="form-check-label" for="id-{{ $language->id }}">
+                                        {{$language->name}}
+                                    </label>
+                                </div>
+                            @endforeach
+    
+                        </div>
+                    </div>
+                    <div class="col-xl-12">
+                        <button type="submit" class="common_btn">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
