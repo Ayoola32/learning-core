19f7f78d102114f2520619dee6572c1ed9f98e61
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-05-01 11:52:33 +0100

    course-content page for step 3 created for view, yet to be modify

---
+++ resources/views/frontend/instructor-dashboard/course/course-content.blade.php
@@ -0,0 +1,9 @@
+@extends('frontend.instructor-dashboard.course.course-app')
+
+@section('course-content')
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+        <div class="add_course_basic_info">
+
+        </div>
+    </div>
+@endsection
