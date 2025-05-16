3d436d3fb161f9992dffd94213fd2997aebf46b8
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-05-16 18:03:17 +0100

    modify index method to fetch courses in descending orderr based on created date

--- app/Http/Controllers/Frontend/CourseController.php
+++ app/Http/Controllers/Frontend/CourseController.php
@@ -24,7 +24,7 @@ class CourseController extends Controller
      */
     public function index()
     {
-        $courses = Course::where('instructor_id', Auth::guard('web')->user()->id)->get();
+        $courses = Course::where('instructor_id', Auth::guard('web')->user()->id)->orderBy('created_at', 'desc')->get();
         return view('frontend.instructor-dashboard.course.index', compact('courses'));
     }
 
