1ece19493935fc6492207631fa98c95d32acd5b4
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-05-21 19:50:15 +0100

    Add chapterLessons relationship to CourseChapter model and update course content view to display lessons

--- app/Models/CourseChapter.php
+++ app/Models/CourseChapter.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CourseChapter extends Model
 {
-    //
+    use HasFactory;
+    public function chapterLessons()
+    {
+        return $this->hasMany(CourseChapterLesson::class, 'course_chapter_id');
+    }
 }
--- resources/views/frontend/instructor-dashboard/course/course-content.blade.php
+++ resources/views/frontend/instructor-dashboard/course/course-content.blade.php
@@ -25,7 +25,9 @@
                                             <i class="far fa-plus"></i>
                                         </div>
                                         <ul class="dropdown-menu dropdown-menu-end">
-                                            <li><a class="dropdown-item add_lesson" href="#" data-chapter-id={{ $chapter->id }} data-course-id={{ $chapter->course_id }}>Add Lesson</a>
+                                            <li><a class="dropdown-item add_lesson" href="#"
+                                                    data-chapter-id={{ $chapter->id }}
+                                                    data-course-id={{ $chapter->course_id }}>Add Lesson</a>
                                             </li>
                                             <li><a class="dropdown-item" href="#">Add Document</a>
                                             </li>
@@ -39,62 +41,20 @@
                             <div id="collapse{{ $chapter->id }}" class="accordion-collapse collapse"
                                 data-bs-parent="#accordionExample">
                                 <div class="accordion-body">
-                                  <ul class="item_list">
-                                    <li>
-                                        <span>Aut autem dolorem debitis mollitia.</span>
-                                        <div class="add_course_content_action_btn">
-                                            
-                                            <a class="edit" href="#"><i class="far fa-edit"></i></a>
-                                            <a class="del" href="#"><i class="fas fa-trash-alt"></i></a>
-                                            <a class="arrow" href="#"><i class="fas fa-arrows-alt"></i></a>
-                                        </div>
-                                    </li>
+                                    <ul class="item_list">
+                                        @foreach ($chapter->chapterLessons as $lesson)
+                                            <li>
+                                                <span>{{ $lesson->title}}</span>
+                                                <div class="add_course_content_action_btn">
 
-                                    <li>
-                                        <span>Aut autem dolorem debitis mollitia.</span>
-                                        <div class="add_course_content_action_btn">
-                                            <a class="edit" href="#"><i class="far fa-edit"></i></a>
-                                            <a class="del" href="#"><i class="fas fa-trash-alt"></i></a>
-                                            <a class="arrow" href="#"><i class="fas fa-arrows-alt"></i></a>
-                                        </div>
-                                    </li>
-                                </ul>
-                                <div class="accordion accordion-flush" id="accordionFlushExample">
-                                    <div class="accordion-item">
-                                        <h2 class="accordion-header">
-                                            <button class="accordion-button collapsed" type="button"
-                                                data-bs-toggle="collapse" data-bs-target="#flush-collapseOne"
-                                                aria-expanded="false" aria-controls="flush-collapseOne">
-                                                <span>Accordion Item #1</span>
-                                            </button>
-                                            <div class="add_course_content_action_btn">
-                                                <div class="dropdown">
-                                                    <div class="btn btn-secondary dropdown-toggle" type="button"
-                                                        data-bs-toggle="dropdown" aria-expanded="false">
-                                                        <i class="far fa-plus"></i>
-                                                    </div>
-                                                    <ul class="dropdown-menu dropdown-menu-end">
-                                                        <li><a class="dropdown-item" href="#">Add Lesson</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="#">Add Document</a>
-                                                        </li>
-                                                        <li><a class="dropdown-item" href="#">Add Quiz</a></li>
-                                                    </ul>
+                                                    <a class="edit" href="#"><i class="far fa-edit"></i></a>
+                                                    <a class="del" href="#"><i class="fas fa-trash-alt"></i></a>
+                                                    <a class="arrow" href="#"><i class="fas fa-arrows-alt"></i></a>
                                                 </div>
-                                                <a class="edit" href="#"><i class="far fa-edit"></i></a>
-                                                <a class="del" href="#"><i class="fas fa-trash-alt"></i></a>
-                                            </div>
-                                        </h2>
-                                        <div id="flush-collapseOne" class="accordion-collapse collapse"
-                                            data-bs-parent="#accordionFlushExample">
-                                            <div class="accordion-body">Placeholder content for
-                                                this accordion, which is intended to demonstrate
-                                                the <code>.accordion-flush</code> class. This is
-                                                the first item's accordion body.</div>
-                                        </div>
-                                    </div>
+                                            </li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                                </div>  
                             </div>
                         </div>
                     @endforeach
