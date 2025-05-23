ab6d88ccfe93b39de33d6625da102cd0d21a6df6
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-05-23 21:18:59 +0100

    add hasOne relation for course model( instructor_id column )

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
+
 {
-    //
+    use HasFactory;
+    function instructor()
+    {
+       return $this->hasOne(User::class, 'id', 'instructor_id');
+    }
+
 }
