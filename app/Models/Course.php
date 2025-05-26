7d71e8bd39b8d60178ddf40d86993f833da613c7
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-05-26 19:30:42 +0100

    modify course model to add other course column relations , like courselvel, courselanguage and coursecategory

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -9,6 +9,7 @@ class Course extends Model
 
 {
     use HasFactory;
+
     function instructor()
     {
        return $this->hasOne(User::class, 'id', 'instructor_id');
@@ -17,6 +18,21 @@ class Course extends Model
     public function courseChapters()
     {
         return $this->hasMany(CourseChapter::class);
+    }
+
+    public function courseCategory()
+    {
+        return $this->belongsTo(CourseCategory::class, 'category_id');
+    }
+
+    public function courseLevel()
+    {
+        return $this->belongsTo(CourseLevel::class, 'course_level_id');
+    }
+
+    public function courseLanguage()
+    {
+        return $this->belongsTo(CourseLanguage::class, 'course_language_id');
     }
 
     public function feedbacks()
