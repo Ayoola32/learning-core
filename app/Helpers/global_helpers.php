a26e165db84801e89908b62c2f4ee383d8e9c4b2
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-06-02 21:11:33 +0100

    add new function to handle the calculatiton of cart courses amount

--- app/Helpers/global_helpers.php
+++ app/Helpers/global_helpers.php
@@ -1,6 +1,9 @@
 <?php
 
 
+use App\Models\Cart;
+
+
 // Convert minutes to hours and minutes
 if (!function_exists('minutesToHoursAndMinutes')) {
     function minutesToHoursAndMinutes(int $minutes) {
@@ -9,4 +12,24 @@ if (!function_exists('minutesToHoursAndMinutes')) {
     return sprintf('%d Hrs  %02d Min', $hours, $remainingMinutes);
     }
 }
-      
+
+// Calculate cart total
+
+if(!function_exists('cartTotal')){
+    function cartTotal(){
+        $total = 0;
+        $cart = Cart::where('user_id', auth()->id())->get();
+
+        foreach ($cart as $item) {
+            if($item->course->discount > 0){
+                // $discountedPrice = $item->course->price - ($item->course->price * ($item->course->discount / 100));
+                $discountedPrice = $item->course->price - $item->course->discount;
+                $total += $discountedPrice;
+            }else {
+                $total += $item->course->price;
+            }
+        }
+
+        return $total;
+    }
+}
