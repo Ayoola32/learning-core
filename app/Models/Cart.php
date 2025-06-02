5fb5197220438bd63082ecff41cca04dd312c61c
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-06-02 20:10:31 +0100

    cart model modified to extends relations to course and users table

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    //Course relationship
+    /**
+     * Get the course that belongs to the cart.
+     */
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    // User relationship
+    /**
+     * Get the user that owns the cart.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
