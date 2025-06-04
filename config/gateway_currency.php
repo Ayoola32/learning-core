68663c101a98f6761f977ce2cda3d07e115b88c2
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-06-04 16:43:20 +0100

    created a config file for paypal currencies listing the currencies in a return array

---
+++ config/gateway_currency.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'paypalCurrencies' => [
+        'USD' => [
+            'name' => 'US Dollar',
+            'symbol' => '$',
+            'code' => 'USD',
+        ],
+        'EUR' => [
+            'name' => 'Euro',
+            'symbol' => '€',
+            'code' => 'EUR',    
+        ],
+        'GBP' => [
+            'name' => 'British Pound',
+            'symbol' => '£',
+            'code' => 'GBP',
+        ],
+        'AUD' => [
+            'name' => 'Australian Dollar',
+            'symbol' => 'A$',
+            'code' => 'AUD',
+        ],
+        'CAD' => [
+            'name' => 'Canadian Dollar',
+            'symbol' => 'CA$',
+            'code' => 'CAD',
+        ],
+        'JPY' => [
+            'name' => 'Japanese Yen',
+            'symbol' => '¥',
+            'code' => 'JPY',
+        ],   
+    ]
+];
